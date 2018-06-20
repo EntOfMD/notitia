736e7e32ef918638ce61ee1dce830165547c4aca
Ashell Fernando <ashellz2010@gmail.com>
2018-06-20 11:21:15 -0400

    implemented google oauth

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 const express = require('express'),
-    app = express(),
-    port = process.env.PORT || 3000,
-    www = process.env.WWW || './';
+	passport = require('passport'),
+	GoogleStrategy = require('passport-google-oauth20').Strategy,
+	keys = require('./config/keys'),
+	app = express(),
+	PORT = process.env.PORT || 3000,
+	www = process.env.WWW || './',
+	morgan = require('morgan');
 
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: keys.googleClientID,
+			clientSecret: keys.googleClientSecret,
+			callbackURL: '/auth/google/callback',
+		},
+		(accessToken, refreshToken, profile, done) => {
+			console.log(`${accessToken}`);
+			console.log(`${refreshToken}`);
+			console.log(profile);
+		}
+	)
+);
 
+app.get('/auth/google/callback', passport.authenticate('google'));
+
+app.get(
+	'/auth/google',
+	passport.authenticate('google', {
+		scope: ['profile', 'email'],
+	})
+);
+
+app.use(morgan('dev'));
 app.use(express.static(www));
 console.log(`serving ${www}`);
-app.get('*', (req, res) => {
-   res.send({hi:'there'});
-});
-app.listen(port, () => console.log(`listening on http://localhost:${port}`));
+
+app.listen(PORT, () => console.log(`listening on http://localhost:${PORT}`));
