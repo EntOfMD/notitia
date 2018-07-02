9f7589ef6b40ddd90cc92f0bb97a7ac523c2cef4
Ashell Fernando <ashellz2010@gmail.com>
2018-07-02 11:13:21 -0400

    added cookie

--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,18 @@ const passport = require('passport'),
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+	console.log('serialized');
+	done(null, user.id); //user.id refers to mongodb's collection id. Now user.id = _id.$oid
+});
+
+passport.deserializeUser((id, done) => {
+	User.findById(id).then(user => {
+		console.log('deserialized');
+		done(null, user);
+	});
+});
+
 passport.use(
 	new GoogleStrategy(
 		{
@@ -12,7 +24,7 @@ passport.use(
 			clientSecret: keys.googleClientSecret,
 			callbackURL: '/auth/google/callback',
 		},
-		(accessToken, refreshToken, profile, done) => {
+		(err, accessToken, refreshToken, profile, done) => {
 			User.findOne({ googleId: profile.id }).then(existingUser => {
 				/* 	const rEmail = [];
 				profile.emails.map(obj => {
@@ -21,23 +33,27 @@ passport.use(
 						console.log(rEmail);
 					}
 				}); */
-
 				if (existingUser) {
 					// if user does exist, do
+					console.log('user exist');
 					done(null, existingUser);
 				} else {
 					// if user does NOT exist, do this
+					console.log('user DOESNT exist');
 					new User({
 						googleId: profile.id,
 						raw: profile._raw,
 						displayName: profile.displayName,
-						emails: profile.emails,
-						name: profile.name,
-						gender: profile.gender,
-						profile: profile,
+						emails: profile._json.emails,
+						name: `${profile.name.givenName} ${profile.name.familyName}`,
+						gender: profile._json.gender || profile.gender,
+						profile: profile._json,
 					})
 						.save()
-						.then(user => done(null, user));
+						.then(user => done(null, user))
+						.catch(error => {
+							console.log(error);
+						});
 				}
 			});
 		}
