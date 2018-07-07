5df737e43660711ed437b5d2cca391e01717bb57
Ashell Fernando <ashellz2010@gmail.com>
2018-07-07 16:42:33 -0400

    update on oath promise - async/await

--- services/passport.js
+++ services/passport.js
@@ -23,38 +23,33 @@ passport.use(
 			callbackURL: '/auth/google/callback',
 			proxy: true, //traffic sets to http instead of https when run thru heroku proxy
 		},
-		(err, accessToken, refreshToken, profile, done) => {
-			User.findOne({ googleId: profile.id }).then(existingUser => {
-				/* 	const rEmail = [];
+		async (err, accessToken, refreshToken, profile, done) => {
+			const existingUser = await User.findOne({ googleId: profile.id });
+			/* 	const rEmail = [];
 				profile.emails.map(obj => {
 					for (var x = 0; x <= rEmail.length; x++) {
 						rEmail[x][obj.value] = obj.type;
 						console.log(rEmail);
 					}
 				}); */
-				if (existingUser) {
-					// if user does exist, do
-					console.log('user exist');
-					done(null, existingUser);
-				} else {
-					// if user does NOT exist, do this
-					console.log('user DOESNT exist');
-					new User({
-						googleId: profile.id,
-						raw: profile._raw,
-						displayName: profile.displayName,
-						emails: profile._json.emails,
-						name: `${profile.name.givenName} ${profile.name.familyName}`,
-						gender: profile._json.gender || profile.gender,
-						profile: profile._json,
-					})
-						.save()
-						.then(user => done(null, user))
-						.catch(error => {
-							console.log(error);
-						});
-				}
-			});
+			if (existingUser) {
+				// if user does exist, do
+				console.log(`${existingUser.name} is already registered!`);
+				done(null, existingUser);
+			} else {
+				// if user does NOT exist, do this
+				const user = await new User({
+					googleId: profile.id,
+					raw: profile._raw,
+					displayName: profile.displayName,
+					emails: profile._json.emails,
+					name: `${profile.name.givenName} ${profile.name.familyName}`,
+					gender: profile._json.gender || profile.gender,
+					profile: profile._json,
+				}).save();
+				console.log(`${user.displayName} has been registered!`);
+				done(null, user);
+			}
 		}
 	)
 );
