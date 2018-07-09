236371dc8d6e63ab72a8cfb8a06d710204946fc8
Ashell Fernando <ashellz2010@gmail.com>
2018-07-09 10:50:55 -0400

    with "/" proxy taken off, new routes work as usual

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-const Header = () => <h2>Header</h2>;
-const Dashboard = () => <h2>Dashboard</h2>;
-const SurveyNew = () => <h2>SurveyNew</h2>;
-const Landing = () => <h2>Landing Page!</h2>;
+import Header from './Header';
+const Dashboard = () => <h2>This will show signed in users their dashboard and info</h2>;
+const SurveyNew = () => <h2>This page will show signed in users to do a new survey</h2>;
+const Landing = () => <h2>Landing Page! this will include sales pitch and data</h2>;
 
 const App = () => {
 	return (
 		<div>
 			<BrowserRouter>
 				<div>
-					<Route path="/" component={Landing} />
+					<Header />
+					<Route exact path="/" component={Landing} />
+					<Route exact path="/app" component={Dashboard} />
+					<Route path="/app/dashboard" component={Dashboard} />
+					<Route path="/app/survey/new" component={SurveyNew} />
+					<Route exact path="/app/survey" component={Dashboard} />
 				</div>
 			</BrowserRouter>
 		</div>
