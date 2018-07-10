370e9d9e7db53719045c6a39e448c6efe1691f65
Ashell Fernando <ashellz2010@gmail.com>
2018-07-10 00:15:53 -0400

    refactored app to class based

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import Header from './Header';
@@ -6,21 +6,25 @@ const Dashboard = () => <h2>This will show signed in users their dashboard and i
 const SurveyNew = () => <h2>This page will show signed in users to do a new survey</h2>;
 const Landing = () => <h2>Landing Page! this will include sales pitch and data</h2>;
 
-const App = () => {
-	return (
-		<div>
-			<BrowserRouter>
-				<div className="container">
-					<Header />
-					<Route exact path="/" component={Landing} />
-					<Route exact path="/app" component={Dashboard} />
-					<Route path="/app/dashboard" component={Dashboard} />
-					<Route path="/app/survey/new" component={SurveyNew} />
-					<Route exact path="/app/survey" component={Dashboard} />
-				</div>
-			</BrowserRouter>
-		</div>
-	);
-};
+class App extends Component {
+	componentDidMount() {}
+
+	render() {
+		return (
+			<div>
+				<BrowserRouter>
+					<div className="container">
+						<Header />
+						<Route exact path="/" component={Landing} />
+						<Route exact path="/app" component={Dashboard} />
+						<Route path="/app/dashboard" component={Dashboard} />
+						<Route path="/app/survey/new" component={SurveyNew} />
+						<Route exact path="/app/survey" component={Dashboard} />
+					</div>
+				</BrowserRouter>
+			</div>
+		);
+	}
+}
 
 export default App;
