782fa46ac6df48bfa0d1f5de29e9f3e3dce6a139
Ashell Fernando <ashellz2010@gmail.com>
2018-07-10 11:48:23 -0400

    Testing out Toasts

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,4 +1,14 @@
 import React from 'react';
+import { ToastContainer, ToastStore } from 'react-toasts';
+
+function Toasts() {
+	return (
+		<div>
+			<button onClick={() => ToastStore.error('Test')}>click me</button>
+			<ToastContainer store={ToastStore} />
+		</div>
+	);
+}
 
 const Landing = () => {
 	return (
