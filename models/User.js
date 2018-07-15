babab15e03003540d916a32bea1e93d1aed5fc75
Ashell Fernando <ashellz2010@gmail.com>
2018-07-15 14:33:29 -0400

    update on user model

--- models/User.js
+++ models/User.js
@@ -2,11 +2,13 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose; //Same as `const Schema = mongoose.Schema`
 
 const userSchema = new Schema({
+	signupDate: { type: Date },
 	googleId: { type: Number },
 	displayName: { type: String },
 	name: [],
 	emails: [],
 	credits: { type: Number, default: 0 },
+	role: { type: String, default: 'consumer' },
 });
 
 mongoose.model('users', userSchema);
