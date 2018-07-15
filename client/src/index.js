dc9897e656385d96c6fefcb7c0d32fd17f00bd69
Ashell Fernando <ashellz2010@gmail.com>
2018-07-15 14:25:43 -0400

    security flaw, removed testing data

--- client/src/index.js
+++ client/src/index.js
@@ -16,5 +16,3 @@ ReactDOM.render(
 	</Provider>,
 	document.querySelector('#root')
 );
-console.log(`STRIPE KEY IS: ${process.env.REACT_APP_STRIPE_PK}`);
-console.log(`ENV IS:${process.env.NODE_ENV} `);
