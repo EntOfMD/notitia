e5fad51b5d5aeac05081b318224166719ab36972
Ashell Fernando <ashellz2010@gmail.com>
2018-07-17 11:35:12 -0400

    added subdocument on mongo

--- models/Survey.js
+++ models/Survey.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const RecipientSchema = require('./Recipient');
 
 const surveySchema = new Schema({
 	title: { type: String },
 	body: { type: String },
 	subject: { type: String },
-	recipients: [String],
+	recipients: [RecipientSchema],
+	yes: { type: Number, default: 0 },
+	no: { type: Number, default: 0 },
 });
 
 mongoose.model('surveys', surveySchema);
