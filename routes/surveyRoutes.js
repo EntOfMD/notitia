9a4056f1dd71a988e755b932f9abca897e7bb492
Ashell Fernando <ashellz2010@gmail.com>
2018-07-17 12:48:29 -0400

    almost finished with surveyRoutes, added more key:values

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,6 +1,25 @@
+const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 
+const Survey = mongoose.model('surveys');
+
 module.exports = app => {
-	app.post('/api/surveys', { requireLogin, requireCredits }, (req, res) => {});
+	app.post('/api/surveys', { requireLogin, requireCredits }, (req, res) => {
+		const { title, subject, body, recipients } = req.body;
+
+		/* 
+		ES6 syntax for identical key:value
+        */
+		const survey = new Survey({
+			title,
+			body,
+			subject,
+			recipients: recipients.split(',').map(email => ({
+				email,
+			})),
+			_user: req.user.id, //id is _id in mongo
+			dateSent: Date.now(),
+		});
+	});
 };
