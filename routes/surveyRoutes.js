5fe2fc4ee173c8387a32a728d59332ad236adfb7
Ashell Fernando <ashellz2010@gmail.com>
2018-07-19 09:56:02 -0400

    fixed error on receiving object instead of callback

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-	app.post('/api/surveys', { requireLogin, requireCredits }, (req, res) => {
+	app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
 		const { title, subject, body, recipients } = req.body;
 
 		/* 
@@ -18,8 +20,9 @@ module.exports = app => {
 			recipients: recipients.split(',').map(email => ({
 				email: email.trim(),
 			})),
-			_user: req.user.id, //id is _id in mongo
+			_user: req.user.id,
 			dateSent: Date.now(),
 		});
+		const mailer = new Mailer(survey, surveyTemplate(survey));
 	});
 };
